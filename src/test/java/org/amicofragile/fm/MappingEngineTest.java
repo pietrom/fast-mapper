d1038c00f5d501d2ccbf271baf0a807082911519
Pietro Martinelli <amicofragile@gmail.com>
2014-07-10 15:55:00 +0200

    Changed variable name

--- src/test/java/org/amicofragile/fm/MappingEngineTest.java
+++ src/test/java/org/amicofragile/fm/MappingEngineTest.java
@@ -9,8 +9,8 @@ public class MappingEngineTest {
 	private static final Person EDDY = new Person("Eddy", "Merckx");
 	@Test
 	public void mapWithoutMappingIsIdentity() throws Exception {
-		final MappingEngine mapper = new MappingEngine();
-		final Object result = mapper.map(EDDY);
+		final MappingEngine engine = new MappingEngine();
+		final Object result = engine.map(EDDY);
 		assertTrue(EDDY == result);
 	}
 }
