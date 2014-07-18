31fa84da4e28b7a2b77753350e66e9af9c4b4115
Pietro Martinelli <amicofragile@gmail.com>
2014-07-18 15:13:42 +0200

    Added 'stringtemplate' to the game: ClassMapper code is generated using 'stringtemplate', non concatenating Strings

--- src/main/java/org/amicofragile/fm/DeclarativeMapperConfiguration.java
+++ src/main/java/org/amicofragile/fm/DeclarativeMapperConfiguration.java
@@ -3,6 +3,11 @@ package org.amicofragile.fm;
 import java.util.Collection;
 import java.util.LinkedList;
 
+import org.stringtemplate.v4.ST;
+import org.stringtemplate.v4.STGroup;
+import org.stringtemplate.v4.STGroupFile;
+import org.stringtemplate.v4.StringRenderer;
+
 public class DeclarativeMapperConfiguration {
 	private final String mapperId;
 	private final Class inClass, outClass;
@@ -24,6 +29,15 @@ public class DeclarativeMapperConfiguration {
 	}
 
 	public String getClassMapperCode() {
+		final ST st = loadTemplateGroup().getInstanceOf("classMapper");
+		st.add("mapperId", mapperId);
+		st.add("mapperInterface", ClassMapper.class.getCanonicalName());
+		st.add("inClass", inClass.getCanonicalName());
+		st.add("outClass", outClass.getCanonicalName());
+		st.add("fieldMappings", fieldMappings);
+		String code = st.render();
+		return code;
+		/*
 		StringBuilder mappingCode = new StringBuilder();
 		mappingCode.append(String.format("final %s out = new %s();", outClass.getCanonicalName(), outClass.getCanonicalName()));
 		for (FieldMapping fm : fieldMappings) {
@@ -33,13 +47,16 @@ public class DeclarativeMapperConfiguration {
 		String code = String.format("package sm;\n public class %s implements %s<%s, %s> {\n public %s map(%s in) {\n %s }\n}", mapperId,
 				ClassMapper.class.getCanonicalName(), inClass.getCanonicalName(), outClass.getCanonicalName(), inClass.getCanonicalName(), outClass.getCanonicalName(), mappingCode.toString());
 		return code;
+		*/
 	}
 
-	private String capitalize(String in) {
-		return in.substring(0, 1).toUpperCase() + in.substring(1);
-	}
-	
 	public String getMapperId() {
 		return mapperId;
+	}
+	
+	private STGroup loadTemplateGroup() {
+		final STGroup group = new STGroupFile(DeclarativeMapperConfiguration.class.getResource("/class-mapper.stg"), "UTF-8", '<', '>');
+		group.registerRenderer(String.class, new StringRenderer());
+		return group;
 	}
 }
